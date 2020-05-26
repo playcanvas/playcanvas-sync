3f9436df17446470f86257f445208d9473061d60
zachpeterpaul <zachpeterpaul@gmail.com>
2020-05-26 11:51:40 -0700

    only handle reg files if reg var set

--- src/utils/type-utils.js
+++ src/utils/type-utils.js
@@ -35,20 +35,29 @@ TEXTUAL_ASSET_TYPES.forEach(t => {
 
 const TypeUtils = {
   isBadFile: function (name, remotePath, conf) {
-    return !TypeUtils.isForceFile(remotePath, conf) &&
-        (conf.PLAYCANVAS_BAD_FILE_REG.test(name) ||
-            !TypeUtils.isTextualFile(name) ||
-            !conf.ignParser.isMatch(remotePath));
+    return TypeUtils.hasForceReg(conf) ?
+        !TypeUtils.isForceFile(remotePath, conf) :
+        TypeUtils.isBadTextual(name, remotePath, conf);
   },
 
+  isBadTextual: function (name, remotePath, conf) {
+    return conf.PLAYCANVAS_BAD_FILE_REG.test(name) ||
+        !TypeUtils.isTextualFile(name) ||
+        !conf.ignParser.isMatch(remotePath);
+  },
 
   isActiveAsset: function(h, conf) {
-    return TypeUtils.isForceFile(h.remotePath, conf) ||
-        (TypeUtils.isTextualType(h.type) &&
-            TypeUtils.isTextualFile(h.name) &&
-            conf.ignParser.isMatch(h.remotePath));
+    return TypeUtils.hasForceReg(conf) ?
+        TypeUtils.isForceFile(h.remotePath, conf) :
+        TypeUtils.isActiveTextual(h, conf);
   },
 
+  isActiveTextual: function(h, conf) {
+    return TypeUtils.isTextualType(h.type) &&
+        TypeUtils.isTextualFile(h.name) &&
+        conf.ignParser.isMatch(h.remotePath);
+  },
+  
   isTextualType: function (t) {
     return TEXTUAL_ASSET_TYPES.includes(t);
   },
@@ -59,10 +68,12 @@ const TypeUtils = {
     return TEXTUAL_EXTENSIONS[ext];
   },
 
-  isForceFile: function (remotePath, conf) {
-    const r = conf.PLAYCANVAS_FORCE_REG;
+  hasForceReg: function (conf) {
+    return conf.PLAYCANVAS_FORCE_REG;
+  },
 
-    return r && r.test(remotePath);
+  isForceFile: function (remotePath, conf) {
+    return conf.PLAYCANVAS_FORCE_REG.test(remotePath);
   }
 };
 
